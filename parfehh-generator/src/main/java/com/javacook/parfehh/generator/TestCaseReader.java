19f079225e1e7067e3159d0aa9bd597025638bf1
Joerg Vollmer <J.Vollmer@verband.creditreform.de>
2016-12-21 16:49:35 +0100

    initial

---
+++ parfehh-generator/src/main/java/com/javacook/parfehh/generator/TestCaseReader.java
@@ -0,0 +1,13 @@
+package com.javacook.parfehh.generator;
+
+import com.javacook.parfehh.domain.TestSeries;
+
+/**
+ * Created by vollmerj on 21.12.16.
+ */
+public interface TestCaseReader {
+
+    TestSeries createTestSeries();
+
+}
+
